19684b7ce2a1770406c46bc62d322b4ebdef0a86
wemui609 <wesley.d.mui@gmail.com>
2021-10-07 22:09:02 -0400

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,38 @@
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #DD0B0B;
+}
+
+li {
+    float: center;
+}
+
+li a {
+    display: block;
+    color: #111; bold;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+    background-color: white;
+}
+</style>
+
+<footer class="w3-center w3-#C1CFDA w3-padding-64" style="background-color:#526D2E;max-width:1900px;margin:auto;">
+  <div class="w3-xlarge" style="background-color:#526D2E;color: black;">
+    <a href="https://www.facebook.com/" class="w3-hover-text-indigo"><i class="fa fa-facebook-official"></i></a>
+    <a href="https://www.pinterest.com/" class="w3-hover-text-red"><i class="fa fa-pinterest-p"></i></a>
+    <a href="https://twitter.com/" class="w3-hover-text-light-blue"><i class="fa fa-twitter"></i></a>
+    <a href="https://www.flickr.com/" class="w3-hover-text-grey"><i class="fa fa-flickr"></i></a>
+    <a href="https://www.linkedin.com/" class="w3-hover-text-indigo"><i class="fa fa-linkedin"></i></a>
+    <a href="index.php" class="w3-hover-text-red" title="Go to top"><i class="fa fa-arrow-up"></i></a>
+  </div>
+  
+</footer>
